2f478f359578d855beac353fb7cab2d5b478e656
Yuri do Monte <yuri.spm@gmail.com>
2020-04-18 11:41:26 -0300

    Python Brasil

--- Python-Brasil/ExerciciosClasses/Python-01.py
+++ Python-Brasil/ExerciciosClasses/Python-01.py
@@ -5,3 +5,21 @@ Classe Bola: Crie uma classe que modele uma bola:
     Métodos: trocaCor e mostraCor 
 """
 
+class Bola():
+    def __init__(self, bola, cor):
+        self.bola = bola
+        self.cor = cor
+
+    def trocarCor(self,cor):
+        self.cor = cor
+    
+    def mostraCor(self):
+        print(f'A cor da bola de {self.bola} e {self.cor}')
+
+
+
+
+b = Bola('Futebol', 'Roxa')
+b.mostraCor()
+b.trocarCor('Amarelo')
+b.mostraCor()
