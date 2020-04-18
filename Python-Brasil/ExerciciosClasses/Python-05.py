797b6231e6465eae92df307dcf99bda42b622eff
Yuri do Monte <yuri.spm@gmail.com>
2020-04-18 17:05:07 -0300

    Python Brasil

--- Python-Brasil/ExerciciosClasses/Python-05.py
+++ Python-Brasil/ExerciciosClasses/Python-05.py
@@ -7,3 +7,39 @@ Classe Conta Corrente: Crie uma classe para implementar uma conta corrente.
 
 
 
+class ContaCorrente:
+    def __init__(self, conta, nome, saldo):
+        self.nconta= conta
+        self.nome = nome
+        self.saldo = saldo
+
+    
+    def alterarNome(self,conta, nome):
+        if conta == self.nconta:
+            self.nome = nome
+        else:
+            print('Conta errada. Por favor informe a conta certa')
+
+
+    
+    def deposito(self, valor):
+        self.saldo += valor
+
+
+    def saque(self, valor):
+        self.saldo -= valor
+
+    
+
+    
+
+client1 = ContaCorrente(875468,'Antonio Vasques', 5000)
+print(client1.nome)
+client1.alterarNome(875468, 'Yuri Santos')
+print()
+print(client1.nome)
+client1.saque(2500)
+print (client1.saldo)
+
+
+
