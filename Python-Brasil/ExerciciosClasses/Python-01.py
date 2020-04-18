446a85178df9eb3a1c19cc512dd183bc6d1bd847
Yuri do Monte <yuri.spm@gmail.com>
2020-04-18 11:01:34 -0300

    Python Brasil

---
+++ Python-Brasil/ExerciciosClasses/Python-01.py
@@ -0,0 +1,7 @@
+"""
+Classe Bola: Crie uma classe que modele uma bola:
+
+    Atributos: Cor, circunferência, material
+    Métodos: trocaCor e mostraCor 
+"""
+
---
+++ Python-Brasil/ExerciciosClasses/Python-02.py
@@ -0,0 +1,7 @@
+"""
+Classe Quadrado: Crie uma classe que modele um quadrado:
+
+    Atributos: Tamanho do lado
+    Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área; 
+"""
+
