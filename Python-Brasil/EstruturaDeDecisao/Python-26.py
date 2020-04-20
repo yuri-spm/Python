e90b571fc2e0fd25f2da969b01dbcedcad610486
Yuri do Monte <yuri.spm@gmail.com>
2020-04-20 13:25:53 -0300

    Pyhon Brasil

--- Python-Brasil/EstruturaDeDecisao/Python-26.py
+++ Python-Brasil/EstruturaDeDecisao/Python-26.py
@@ -11,3 +11,28 @@ Um posto está vendendo combustíveis com a seguinte tabela de descontos:
     tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago
     pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
 """
+
+resp = int(input('ESCOLHA: \n1 - Alcool \n2 - Gasolina \nOpção:'))
+
+
+if resp == 1:
+    qlitros = int(input('Informe a quantidade de litros: '))
+    if qlitros <= 20:
+        tlitros = qlitros * 1.9 
+        tlitros = tlitros - (tlitros *3/100)
+        print(f'TOTAL: {tlitros:.2f}')
+    else:
+        tlitros = qlitros * 1.9  
+        tlitros = tlitros - (tlitros *5/100)
+        print(f'TOTAL: {tlitros:.2f}')
+        tlitros = qlitros * 1.9
+elif resp == 2:
+    qlitros = int(input('Informe a quantidade de litros: '))
+    if qlitros <= 20:
+        tlitros = qlitros * 2.5
+        tlitros = tlitros - (tlitros *4/100)
+        print(f'TOTAL: {tlitros:.2f}')
+    else:
+        tlitros = qlitros * 2.5
+        tlitros = tlitros - (tlitros * 6/100)
+        print(f'TOTAL: {tlitros:.2f}')
