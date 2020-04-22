65e1c9bd02bdf0c589fcb8f7c1504fcc34e69271
Yuri do Monte <yuri.spm@gmail.com>
2020-04-22 11:42:40 -0300

    Python Brasil

--- Python-Brasil/EstruturaDeRepeticao/Python-24.py
+++ Python-Brasil/EstruturaDeRepeticao/Python-24.py
@@ -1,3 +1,10 @@
 #Faça um programa que calcule o mostre a média aritmética de N notas.
 
 
+soma = 0
+quant= int(input('Informe a quantidade de notas: '))
+for c in range(0, quant):
+    n = float(input('Digite a primeira nota: '))
+    soma +=n
+
+print(f'A media das notas foi {(soma/quant):.1f}')
--- Python-Brasil/EstruturaDeRepeticao/Python-25.py
+++ Python-Brasil/EstruturaDeRepeticao/Python-25.py
@@ -1,1 +1,17 @@
+"""
 Faça um programa que peça para n pessoas a sua idade, ao final o programa devera verificar se a média de idade da turma varia entre 0 e 25,26 e 60 e maior que 60; e então, dizer se a turma é jovem, adulta ou idosa, conforme a média calculada. 
+"""
+
+lista = list()
+n = int(input('Digite a quantidades de pessoas entrevistadas: '))
+for c in range(0, n):
+    resp = int(input('Digite sua idade: '))
+    lista.append(resp)
+
+media = sum(lista)/len(lista)
+if media <= 25:
+    print('Jovem')
+elif media > 26 or media < 60:
+    print('Adulto')
+else:
+    print('Idoso')
