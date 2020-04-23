60b2eb45ecde5ab8f30d03edbad3a095fe371f4b
Yuri do Monte <yuri.spm@gmail.com>
2020-04-23 12:08:09 -0300

    Python Brasil

--- Python-Brasil/EstruturaDeRepeticao/Python-24.py
+++ Python-Brasil/EstruturaDeRepeticao/Python-24.py
@@ -7,15 +7,6 @@ for c in range(0, quant):
     soma +=n
 
 
-soma = 0
-quant= int(input('Informe a quantidade de notas: '))
-for c in range(0, quant):
-    n = float(input('Digite a primeira nota: '))
-    soma +=n
 
+print(f'A media das notas é: {(soma/quant):.2f}')
 
-<<<<<<< HEAD
-print(f'A media das notas foi {(soma/quant):.1f}')
-=======
-print(f'A media das notas foi {(soma/quant):.1f}')
->>>>>>> b7be70ec852e411e7c84eba2078c88e7e311f862
