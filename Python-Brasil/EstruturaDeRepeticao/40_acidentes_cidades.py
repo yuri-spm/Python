321571a950f1a628e26d115f73cbc5fd7d448a88
Yuri do Monte <yuri.spm@gmail.com>
2020-04-30 17:52:02 -0300

    Python Brasil

--- Python-Brasil/EstruturaDeRepeticao/40_acidentes_cidades.py
+++ Python-Brasil/EstruturaDeRepeticao/40_acidentes_cidades.py
@@ -1,4 +1,6 @@
-Foi feita uma estatística em cinco cidades brasileiras para coletar dados sobre acidentes de trânsito. Foram obtidos os seguintes dados:
+"""
+Foi feita uma estatística em cinco cidades brasileiras para coletar dados sobre acidentes de trânsito. 
+Foram obtidos os seguintes dados:
 
     Código da cidade;
     Número de veículos de passeio (em 1999);
@@ -6,3 +8,29 @@ Foi feita uma estatística em cinco cidades brasileiras para coletar dados sobre
     Qual o maior e menor índice de acidentes de transito e a que cidade pertence;
     Qual a média de veículos nas cinco cidades juntas;
     Qual a média de acidentes de trânsito nas cidades com menos de 2.000 veículos de passeio. 
+"""
+
+cidade = []
+cadastro = {}
+for c in range(0, 2):
+    cadastro['Cidade'] = int(input('Codigo da Cidade: '))
+    cadastro['Veiculos'] = int(input('Quantidade Veiculos de passeio: '))
+    cadastro['Acidentes'] = int(input('Quantidades de acidentes em 1999: '))
+    cidade.append(cadastro.copy)
+    cadastro.clear
+
+
+print()
+
+for info in cadastro.keys() :
+    print(f'{info:>2}\t\t ', end='')
+print()
+
+for k, v in enumerate(cadastro):
+    for n in v.values():
+        print(f'{str(n):<15}', end='')
+    print()
+print('-=-'*20)
+
+
+
